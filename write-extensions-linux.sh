0a8c6f699050995c298dc14102e6c1055f6912f3
skewb1k <artem.krinitsyn@gmail.com>
2024-09-09 05:36:18 +0300

    sh -> bash scripts

--- write-extensions-linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "#!/bin/sh" > install-extensions.sh
-code --list-extensions | xargs -L 1 echo code --install-extension >> install-extensions.sh
--- write-extensions-win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "#!/bin/sh" > install-extensions.sh
-code --list-extensions | % { "code --install-extension $_" } >> install-extensions.sh
